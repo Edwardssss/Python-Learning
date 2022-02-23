af2a4074e9dd74305d77e6346170736cbef99ad1
Edwardssss <64591003+Edwardssss@users.noreply.github.com>
2022-02-23 19:25:18 +0800

    Create SQLite.py

---
+++ 操作数据库/SQLite.py
@@ -0,0 +1,16 @@
+# python当中有sqlite3库以供操作数据库
+# 下面演示创建数据库并存储文件
+
+import sqlite3
+
+database = sqlite3.connect('MySQL_1')  # 链接数据库，如果数据库不存在则创建一个
+cursor = database.cursor()  # 创建一个cursor
+# 插入数据
+cursor.execute('insert into user (id ,name) values ("1","Ed")')
+cursor.execute('insert into user (id ,name) values ("2","Dan")')
+cursor.execute('insert into user (id ,name) values ("3","Eric")')
+cursor.close()  # 关闭游标
+database.commit()  # 提交实务
+database.close()  # 关闭connection
+
+# 使用完数据库之后要记得关闭connection和cursor
